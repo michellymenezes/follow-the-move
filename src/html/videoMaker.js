14be5a226092f6dcb6b552d3a4b3355910159e50
maysa macedo <maysa.macedo95@gmail.com>
2016-03-08 23:49:53 -0300

    separando html e js

---
+++ src/html/videoMaker.js
@@ -0,0 +1,99 @@
+function captureUserMedia(mediaConstraints, successCallback, errorCallback) {
+    navigator.mediaDevices.getUserMedia(mediaConstraints).then(successCallback).catch(errorCallback);
+}
+var mediaConstraints = {
+    audio: !IsChrome && !IsOpera && !IsEdge, // record both audio/video in Firefox
+    video: true
+};
+document.querySelector('#start-recording').onclick = function() {
+    this.disabled = true;
+    captureUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
+};
+document.querySelector('#stop-recording').onclick = function() {
+    this.disabled = true;
+    mediaRecorder.stop();
+    mediaRecorder.stream.stop();
+    document.querySelector('#pause-recording').disabled = true;
+    document.querySelector('#start-recording').disabled = false;
+};
+document.querySelector('#pause-recording').onclick = function() {
+    this.disabled = true;
+    mediaRecorder.pause();
+    document.querySelector('#resume-recording').disabled = false;
+};
+document.querySelector('#resume-recording').onclick = function() {
+    this.disabled = true;
+    mediaRecorder.resume();
+    document.querySelector('#pause-recording').disabled = false;
+};
+document.querySelector('#save-recording').onclick = function() {
+    this.disabled = true;
+    mediaRecorder.save();
+};
+var mediaRecorder;
+function onMediaSuccess(stream) {
+    var video = document.createElement('video');
+    var videoWidth = document.getElementById('video-width').value || 320;
+    var videoHeight = document.getElementById('video-height').value || 240;
+    video = mergeProps(video, {
+        controls: true,
+        muted: true,
+        width: videoWidth,
+        height: videoHeight,
+        src: URL.createObjectURL(stream)
+    });
+    video.play();
+    videosContainer.appendChild(video);
+    videosContainer.appendChild(document.createElement('hr'));
+    mediaRecorder = new MediaStreamRecorder(stream);
+    mediaRecorder.stream = stream;
+    mediaRecorder.mimeType = 'video/webm'; // this line is mandatory
+    mediaRecorder.videoWidth = videoWidth;
+    mediaRecorder.videoHeight = videoHeight;
+    mediaRecorder.ondataavailable = function(blob) {
+        var a = document.createElement('a');
+        a.target = '_blank';
+        a.innerHTML = 'Open Recorded Video No. ' + (index++) + ' (Size: ' + bytesToSize(blob.size) + ') Time Length: ' + getTimeLength(timeInterval);
+        a.href = URL.createObjectURL(blob);
+        videosContainer.appendChild(a);
+        videosContainer.appendChild(document.createElement('hr'));
+
+           // var form = document.createElement('form');
+            // var mediaElement = document.createElement('video');
+            // mediaElement.autoplay = true;
+            // mediaElement.controls = true;
+            // mediaElement.src = URL.createObjectURL(blob.video);
+            // mediaElement.style.width= '100%';
+            // videosContainer.appendChild(mediaElement);
+
+    };
+    var timeInterval = document.querySelector('#time-interval').value;
+    if (timeInterval) timeInterval = parseInt(timeInterval);
+    else timeInterval = 5 * 1000;
+    // get blob after specific time interval
+    mediaRecorder.start(timeInterval);
+    document.querySelector('#stop-recording').disabled = false;
+    document.querySelector('#pause-recording').disabled = false;
+    document.querySelector('#save-recording').disabled = false;
+}
+function onMediaError(e) {
+    console.error('media error', e);
+}
+var videosContainer = document.getElementById('videos-container');
+var index = 1;
+// below function via: http://goo.gl/B3ae8c
+function bytesToSize(bytes) {
+    var k = 1000;
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return '0 Bytes';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(k)), 10);
+    return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+}
+// below function via: http://goo.gl/6QNDcI
+function getTimeLength(milliseconds) {
+    var data = new Date(milliseconds);
+    return data.getUTCHours() + " hours, " + data.getUTCMinutes() + " minutes and " + data.getUTCSeconds() + " second(s)";
+}
+window.onbeforeunload = function() {
+    document.querySelector('#start-recording').disabled = false;
+};
