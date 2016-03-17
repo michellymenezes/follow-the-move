9c30476eb85d5d41fcfb3a96fb97f4c3393ceee9
rinaldomelo <rinaldomelo72@gmail.com>
2016-03-17 01:34:39 -0300

    adding folders

--- src/html/upload_file.php
+++ src/html/upload_file.php
@@ -18,19 +18,19 @@ if ((($_FILES["file"]["type"] == "video/mp4")
     }
   else
     {
-    echo "Folder: " . $_REQUEST["folder"] . "<br />";
+    echo "Folder Name: " . $_POST["folder"] . "<br />";
 
     echo "Sucess";
       
-    if (file_exists("upload/" . $_FILES["file"]["name"]))
+    if (file_exists("upload/" . $_POST["folder"] . "/" . $_FILES["file"]["name"]))
       {
       echo $_FILES["file"]["name"] . " already exists. ";
       }
     else
       {
       move_uploaded_file($_FILES["file"]["tmp_name"],
-      "upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      "upload/" . $_POST["folder"] . "/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "upload/" . $_POST["folder"] . "/". $_FILES["file"]["name"];
       }
     }
   }
