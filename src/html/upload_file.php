1ec23ef21f529bdabfcda1ab5b0c68b07c6665b8
rinaldomelo <rinaldomelo72@gmail.com>
2016-03-17 01:04:43 -0300

    adding form for kaoma

--- src/html/upload_file.php
+++ src/html/upload_file.php
@@ -18,13 +18,10 @@ if ((($_FILES["file"]["type"] == "video/mp4")
     }
   else
     {
-    echo "Folder: " . $_REQUEST["file"]["name"] . "<br />";
+    echo "Folder: " . $_REQUEST["folder"] . "<br />";
 
-    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-    echo "Type: " . $_FILES["file"]["type"] . "<br />";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-
+    echo "Sucess";
+      
     if (file_exists("upload/" . $_FILES["file"]["name"]))
       {
       echo $_FILES["file"]["name"] . " already exists. ";
